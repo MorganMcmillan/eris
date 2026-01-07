fce534a49c1316ef35b42ec2415fdccbfc24b355
MorganMcmillan <mcmillan.a.morgan@gmail.com>
2026-01-07 09:36:38 -0700

    Feat: replace manual tokenizer with Logos

--- src/token.rs
+++ src/token.rs
@@ -1,126 +1,227 @@
-use std::ops::Range;
+use logos::Logos;
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub enum TokenType {
+#[derive(Logos, Clone, Copy, Debug, PartialEq, Eq)]
+#[logos(skip r" \t\r\n\f")]
+pub enum Token<'a> {
     // Single character tokens
+    #[token(",")]
     Comma,
+    #[token("$")]
     Dollar,
+    #[token("@")]
     At,
+    #[token("#")]
     Hash,
+    #[token("?")]
     Question,
+    #[token("!")]
     Bang,
+    #[token("~")]
     Tilde,
     // Matching pairs of tokens
+    #[token(".")]
     Dot,
+    #[token("..")]
     DotDot,
+    #[token("...")]
     DotDotDot,
+    #[token("..=")]
     DotDotEquals,
+    #[token(":")]
     Colon,
+    #[token("::")]
     ColonColon,
+    #[token(":>")]
     ColonGreater,
+    #[token(":>>")]
     ColonGreaterGreater,
+    #[token("<<")]
     LessLess,
     GreaterGreater, // Note: not scanned directly, but constructed by the parser during parsing
-    // Potential multi character tokens
+    // Balanced pairs
+    #[token("(")]
     LeftParen,
+    #[token(")")]
     RightParen,
+    #[token("[")]
     LeftBracket,
+    #[token("]")]
     RightBracket,
+    #[token("\x7b")]
     LeftBrace,
+    #[token("}")]
     RightBrace,
     // Operators with assignment variants
+    #[token("+")]
     Plus,
+    #[token("+=")]
     PlusEquals,
+    #[token("++")]
     PlusPlus,
+    #[token("++=")]
     PlusPlusEquals,
+    #[token("-")]
     Minus,
+    #[token("-=")]
     MinusEquals,
+    #[token("*")]
     Star,
+    #[token("*=")]
     StarEquals,
+    #[token("**")]
     StarStar,
+    #[token("**=")]
     StarStarEquals,
+    #[token("/")]
     Slash,
+    #[token("/=")]
     SlashEquals,
+    #[token("%")]
     Percent,
+    #[token("%=")]
     PercentEquals,
+    #[token("=")]
     Equals,
+    #[token("==")]
     EqualsEquals,
+    #[token("!=")]
     BangEquals,
+    #[token("<")]
     Less,
+    #[token("<=")]
     LessEquals,
+    #[token(">")]
     Greater,
+    #[token(">=")]
     GreaterEquals,
+    #[token("|")]
     Pipe,
+    #[token("|=")]
     PipeEquals,
+    #[token("&")]
     Ampersand,
+    #[token("&=")]
     AmpersandEquals,
+    #[token("^")]
     Carat,
+    #[token("^=")]
     CaratEquals,
     // Tokens than may repeat
     // Keywords
+    #[token("abstract")]
     Abstract,
+    #[token("and")]
     And,
+    #[token("as")]
     As,
+    #[token("break")]
     Break,
+    #[token("class")]
     Class,
+    #[token("const")]
     Const,
+    #[token("continue")]
     Continue,
+    #[token("do")]
     Do,
+    #[token("else")]
     Else,
+    #[token("enum")]
     Enum,
+    #[token("extend")]
     Extend,
+    #[token("false")]
     False,
+    #[token("fn")]
     Fn,
+    #[token("for")]
     For,
+    #[token("loop")]
     Loop,
+    #[token("if")]
     If,
+    #[token("in")]
     In,
+    #[token("interface")]
     Interface,
+    #[token("is")]
     Is,
+    #[token("let")]
     Let,
+    #[token("macro")]
     Macro,
+    #[token("match")]
     Match,
+    #[token("mixin")]
     Mixin,
+    #[token("never")]
     Never,
+    #[token("nil")]
     Nil,
+    #[token("not")]
     Not,
+    #[token("of")]
     Of,
+    #[token("or")]
     Or,
+    #[token("return")]
     Return,
+    #[token("self")]
     LowercaseSelf,
+    #[token("Self")]
     UppercaseSelf,
+    #[token("static")]
     Static,
+    #[token("super")]
     LowercaseSuper,
+    #[token("Super")]
     UppercaseSuper,
+    #[token("true")]
     True,
+    #[token("type")]
     Type,
+    #[token("unless")]
     Unless,
+    #[token("until")]
     Until,
+    #[token("use")]
     Use,
+    #[token("while")]
     While,
+    #[token("with")]
     With,
     // Literals
-    Identifier,
+    #[regex(r"([a-zA-Z_]\w*)", |lex| lex.slice())]
+    Identifier(&'a str),
     String,
+    // TODO: Implement char and string parsing
+    #[regex(r"'(\\|.)'")]
     Char,
     // Number literals
     Base64,
     Base36,
     Base32,
+    #[regex(r"0h[0-9a-fA-F_]+")]
     Hexadecimal,
+    #[regex(r"\d[\d_]*")]
+    #[regex(r"0d[\d_]+")]
     Decimal,
+    #[regex(r"0o[0-7_]+")]
     Octal,
+    #[regex(r"0q[0-3_]+")]
     Quadal,
+    #[regex(r"0b[01_]+")]
     Binary,
     // Other
+    #[token("->")]
     MinusArrow,
     Error,
     Eof,
 }
 
-impl TokenType {
+impl Token {
     pub fn is_assignment_operator(&self) -> bool {
-        use TokenType::*;
+        use Token::*;
         matches!(self,
             Equals
             | PlusEquals
@@ -137,7 +238,7 @@ impl TokenType {
     }
 
     pub fn infix_binding_power(&self) -> Option<(u8, u8)> {
-        use TokenType::*;
+        use Token::*;
         let bp = match *self {
             Or => (1, 2),
             And => (3, 4),
@@ -162,7 +263,7 @@ impl TokenType {
     }
 
     pub fn prefix_binding_power(&self) -> Option<((), u8)> {
-        use TokenType::*;
+        use Token::*;
         // After As
         match *self {
             Not | Minus | Tilde | Ampersand => Some(((), 30)),
@@ -171,7 +272,7 @@ impl TokenType {
     }
     
     pub fn postfix_binding_power(&self) -> Option<(u8, ())> {
-        use TokenType::*;
+        use Token::*;
         match *self {
             // after unary
             Question | Bang => Some((31, ())),
@@ -179,33 +280,5 @@ impl TokenType {
             Dot | LeftParen | LeftBracket => Some((34, ())),
             _ => None
         }
-    }
-}
-
-#[derive(Clone, Copy, Debug)]
-pub struct Token<'a> {
-    pub token_type: TokenType,
-    pub lexeme: &'a str,
-}
-
-impl<'a> Token<'a> {
-    pub fn new(token_type: TokenType, lexeme: &'a str) -> Self {
-        Self {
-            token_type,
-            lexeme
-        }
-    }
-
-    pub fn span(&self, input: &'a str) -> Range<usize> {
-        let starting_address = input.as_ptr() as usize;
-        let start = self.lexeme.as_ptr() as usize - starting_address;
-        let end = start + self.lexeme.len();
-        return start..end;
-    }
-
-    // Check if this token sits right next to another token (meaning there's no whitespace separating them)
-    pub fn is_next_to(&self, other: &Token) -> bool {
-        let this_address = self.lexeme.as_ptr() as usize + self.lexeme.len();
-        return this_address == other.lexeme.as_ptr() as usize;
     }
 }
